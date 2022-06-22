dd8c1a2d1f6bf573d54062f51e3b1162c7b21cfb
Isaiah White <whiteisaiah343@gmail.com>
2022-06-22 19:19:43 -0400

    updated files

--- backend/src/config.js
+++ backend/src/config.js
@@ -69,7 +69,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = ["0xC7587D5Dea3206e0249c657E4AC9548BD51dcb2f"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0x52a706f17373Da7e1352b94a95b6DE37bE532801"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
